bafe8ef8126d8095d5400d09631c847764a5159a
FrontierUI <uifrontier95@gmail.com>
2024-11-13 13:09:30 +0500

    navbar respons

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 const App = () => {
   return (
     <main className="relative wrappered z-[1] w-full max-w-full h-full">
-      <div className="flexy w-full">
+      <div className="flexy w-full h-full">
         <Navbar />
       </div>
 
