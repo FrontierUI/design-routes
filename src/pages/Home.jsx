9c1ef89c938c49579f6c030a084157157e7c2618
FrontierUI <uifrontier95@gmail.com>
2025-01-08 23:02:15 +0500

    auth pages error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
+// import React from 'react';
 
 import Hero from '../components/Hero';
 import VideosCarousel from '../components/VideosCarousel';
-
-// import VideosCarousel from '@/src';
 
 const Home = () => {
   return (
     <div className="relative max-w-full w-full h-full">
       <Hero />
+
       <div className="container w-full h-full px-5 lg:px-12">
         <VideosCarousel />
-        {/* <VideosCarouselFixed /> */}
       </div>
     </div>
   );
