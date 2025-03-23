c9fa891f0c3a8030ff1d6fd9c2e23b31dc159793
FrontierUI <uifrontier95@gmail.com>
2025-03-23 16:22:44 +0500

    portflos

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const PortfolioPage = () => {
-  return <div className="relative w-full">PortfolioPage</div>;
+  return (
+    <div className="relative w-full h-full caseStudies">
+      <div className="flexy w-full h-full px-5 lg:px-12">
+        <div className="flexy w-full h-full relative py-6 mt-14">
+          <img src="" className="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PortfolioPage;
