a167e35a7b2e0e5b388561219b39633d8d971ad9
FrontierUI <uifrontier95@gmail.com>
2025-05-22 19:13:26 +0500

    logo maquee logos changed

--- src/components/LogoMarquee.jsx
+++ src/components/LogoMarquee.jsx
@@ -7,15 +7,6 @@ import LazyImage from './LazyImage';
 const LogoMarquee = () => {
   return (
     <div className="client-logo w-full overflow-x-hidden masking min-h-20">
-      {/* {clientsLogo.map((cnLog) => (
-    <div className="mx-4 flexy" key={cnLog.cLogoId}>
-      <LazyImage
-        src={cnLog.src}
-        className="img-fluid min-w-32 max-w-40"
-        alt=""
-      />
-    </div>
-  ))} */}
       <Marquee
         direction="right"
         autoFill={true}
@@ -27,139 +18,15 @@ const LogoMarquee = () => {
         pauseOnHover={false}
         className="w-full gap-4 flex items-center bg-transparent"
       >
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workair.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workasc.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workbayer.png"
-            alt=""
-            className="img-fluid h-16 w-16"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workbeachfox.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workbembien.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workcreactco.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workcsquad.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workfaizasteel.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workfaw.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workgetz.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workicipak.png"
-            alt=""
-            className="img-fluid h-16 w-20"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workiil.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workinverex.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workmazito.png"
-            alt=""
-            className="img-fluid h-16 w-20"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workmoosh.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/worknixon.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workrubies.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/worksoyoung.png"
-            alt=""
-            className="img-fluid h-16 w-40"
-          />
-        </div>
-        <div className="flexy mx-4">
-          <LazyImage
-            src="/images/clientLogo/workup.png"
-            alt=""
-            className="img-fluid h-16 w-20"
-          />
-        </div>
+        {clientsLogo.map((cnLog) => (
+          <div className="mx-4 flexy" key={cnLog.cLogoId}>
+            <LazyImage
+              src={cnLog.src}
+              className="img-fluid max-w-36 max-h-[72px]"
+              alt=""
+            />
+          </div>
+        ))}
       </Marquee>
     </div>
   );
