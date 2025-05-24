898f50ad766bafe586238a61f2f2bfb41a8952a0
LionCreativeDev <brave_lion_08@yahoo.com>
2025-05-25 03:17:56 +0500

    api integrated in '/dashboard/overview' for client and admin

--- src/components/OverviewDetails.jsx
+++ src/components/OverviewDetails.jsx
@@ -5,40 +5,46 @@ import {
   LifeBuoy,
   Mail,
   NotepadText,
+  Package,
   Settings,
   Ticket,
-} from 'lucide-react';
+  Users,
+} from "lucide-react";
+import { Link } from "react-router-dom";
 
-const OverviewDetails = () => {
+const OverviewDetails = ({ Role, DashboardData }) => {
   //
 
   const userDetails = [
-    {
-      icon: <History className="w-7 h-7" />,
-      name: 'View Order History',
-      detaling: '3 active of 4 Orders', // yeh dynamic hoga paayen
-    },
-    {
-      icon: <NotepadText className="w-7 h-7" />,
-      name: 'Manage Your Invoices',
-      detaling: '1 active of 4 Orders', // yeh dynamic hoga paayen
-    },
-    {
-      icon: <Ticket className="w-7 h-7" />,
-      name: 'Manage Ticket',
-      detaling: '2 active of 4 Orders', // yeh dynamic hoga paayen
-    },
+    // {
+    //   icon: <History className="w-7 h-7" />,
+    //   name: "View Order History",
+    //   detaling: "3 active of 4 Orders", // yeh dynamic hoga paayen
+    // },
+    // {
+    //   icon: <NotepadText className="w-7 h-7" />,
+    //   name: "Manage Your Invoices",
+    //   detaling: "1 active of 4 Orders", // yeh dynamic hoga paayen
+    // },
+    // {
+    //   icon: <Ticket className="w-7 h-7" />,
+    //   name: "Manage Ticket",
+    //   detaling: "2 active of 4 Orders", // yeh dynamic hoga paayen
+    // },
     {
       icon: <Settings className="w-7 h-7" />,
-      name: 'Settings',
+      name: "Settings",
+      url: "/dashboard/user-profile",
     },
     {
       icon: <Mail className="w-7 h-7" />,
-      name: 'Contact',
+      name: "Contact",
+      url: "/dashboard/contact",
     },
     {
       icon: <LifeBuoy className="w-7 h-7" />,
-      name: 'Help Center',
+      name: "Help Center",
+      url: "/dashboard/help-center",
     },
   ];
 
@@ -46,51 +52,92 @@ const OverviewDetails = () => {
   const overviewDetails = [
     {
       icon: <History className="w-7 h-7" />,
-      name: 'View Order History',
-      detaling: '3 active of 4 Orders', // yeh dynamic hoga paayen
+      name: "Order",
+      detaling: <p>Total Orders: <span className="font-monaBold text-lg">{DashboardData.total_orders}</span></p>,//`${DashboardData.total_orders}`,//"3 active of 4 Orders", // yeh dynamic hoga paayen
     },
     {
-      icon: <NotepadText className="w-7 h-7" />,
-      name: 'Manage Your Invoices',
-      detaling: '1 active of 4 Orders', // yeh dynamic hoga paayen
+      icon: <Users className="w-7 h-7" />,
+      name: "Users",
+      detaling: <p>Total Users: <span className="font-monaBold text-lg">{DashboardData.total_users}</span></p>,//`${DashboardData.total_users}`,//"1 active of 4 Orders", // yeh dynamic hoga paayen
     },
     {
       icon: <Ticket className="w-7 h-7" />,
-      name: 'Manage Ticket',
-      detaling: '2 active of 4 Orders', // yeh dynamic hoga paayen
+      name: "Ticket",
+      detaling: <p>Total Tickets: <span className="font-monaBold text-lg">{DashboardData.total_tickets}</span></p>,//`${DashboardData.total_tickets}`,//"2 active of 4 Orders", // yeh dynamic hoga paayen
     },
     {
-      icon: <Settings className="w-7 h-7" />,
-      name: 'Settings',
+      icon: <Ticket className="w-7 h-7" />,
+      name: "Services",
+      detaling: <p>Total Services: <span className="font-monaBold text-lg">{DashboardData.total_services}</span></p>,//`${DashboardData.total_services}`,//"2 active of 4 Orders", // yeh dynamic hoga paayen
     },
     {
-      icon: <Mail className="w-7 h-7" />,
-      name: 'Contact',
+      icon: <Package className="w-7 h-7" />,
+      name: "Packages",
+      detaling: <p>Total Packages: <span className="font-monaBold text-lg">{DashboardData.total_packages}</span></p>,//`${DashboardData.total_packages}`,//"2 active of 4 Orders", // yeh dynamic hoga paayen
     },
-    {
-      icon: <LifeBuoy className="w-7 h-7" />,
-      name: 'Help Center',
-    },
+    // {
+    //   icon: <Settings className="w-7 h-7" />,
+    //   name: "Settings",
+    // },
+    // {
+    //   icon: <Mail className="w-7 h-7" />,
+    //   name: "Contact",
+    // },
+    // {
+    //   icon: <LifeBuoy className="w-7 h-7" />,
+    //   name: "Help Center",
+    // },
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full mx-auto">
-      {userDetails.map((detail) => (
-        <div
-          key={detail.name}
-          className="justStartCenter flex-col shadow-drop-5 overflow-hidden rounded-lg w-full h-full"
-        >
-          <div className="bgChange w-full h-full p-6 space-y-6 rounded-lg">
-            <div className="flexy bg-primary text-white rounded-full w-12 h-12 p-1">
-              {detail.icon}
-            </div>
-            <div className="flex flex-col space-y-1">
-              <h2 className="text-2xl font-monaSemibold">{detail.name}</h2>
-              <p className="text-sm mb-2 font-monaLight">{detail.detaling}</p>
-            </div>
+    <div
+      className={
+        Role === "admin"
+          ? "grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-5 gap-6 w-full mx-auto"
+          : "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full mx-auto"
+      }
+    >
+      {/* className={`grid ${Role === "admin" ? 'grid-cols-2' : 'grid-cols-1'} sm:grid-cols-2 ${Role === "admin" ? 'lg:grid-cols-5' : 'lg:grid-cols-3'} gap-6 w-full mx-auto`} */}
+      {(Role === "admin" ? [...overviewDetails] : [...userDetails]).map(
+        (detail, index) => (
+          <div
+            key={index}
+            className="justStartCenter flex-col shadow-drop-5 overflow-hidden rounded-lg w-full h-full"
+          >
+            {detail.url !== undefined ? (
+              <Link
+                to={detail.url}
+                className="bgChange w-full h-full p-6 space-y-6 rounded-lg"
+              >
+                <div className="flexy bg-primary text-white rounded-full w-12 h-12 p-1">
+                  {detail.icon}
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <h2 className="text-2xl font-monaSemibold">{detail.name}</h2>
+                  <p className="text-sm mb-2 font-monaLight">
+                    {detail.detaling}
+                  </p>
+                </div>
+              </Link>
+            ) : (
+              <div
+                to={detail.url}
+                className="bgChange w-full h-full p-6 space-y-6 rounded-lg"
+              >
+                <div className="flexy bg-primary text-white rounded-full w-12 h-12 p-1">
+                  {detail.icon}
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <h2 className="text-2xl font-monaSemibold">{detail.name}</h2>
+                  <p className="text-sm mb-2 font-monaLight">
+                    {detail.detaling}
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
-        </div>
-      ))}
+        )
+      )}
     </div>
   );
 };
--- src/components/ProfileOverview.jsx
+++ src/components/ProfileOverview.jsx
@@ -1,36 +1,62 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import { FileDown, Package, Pencil, Ticket } from 'lucide-react';
+import { FileDown, Package, Pencil, Ticket } from "lucide-react";
+import { useEffect, useState } from "react";
 
-const ProfileOverview = () => {
+const ProfileOverview = ({ TotalOrders, TotalTickets }) => {
+  const [userDetails, setUserDetails] = useState({});
+
+  useEffect(() => {
+    let loginSecret = window.localStorage.getItem("loginSecret");
+    if (loginSecret !== undefined) {
+      setUserDetails(JSON.parse(atob(atob(loginSecret))));
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   console.log("userDetails", userDetails);
+  // }, [userDetails]);
+
   return (
     <div className="grid grid-cols-12 grid-flow-dense gap-6 w-full h-full  mx-auto">
-      <div className="md:row-span-3 col-span-12 md:col-span-6 w-full bg-white relative rounded-lg">
+      <div className="md:row-span-2 col-span-12 md:col-span-6 w-full bg-white relative rounded-lg">
         <div className="sm:p-5 p-3 flex flex-col relative w-full">
           <div className="relative flex items-start justify-between w-full">
             <div className="flex items-center gap-3">
-              <img
-                src="/images/icons/ProfAvatar.svg"
-                className="img-fluid w-12"
-                alt=""
-              />
+              {userDetails.profile_picture !== null &&
+              userDetails.profile_picture !== undefined &&
+              userDetails.profile_picture !== "" ? (
+                <img
+                  src={`${import.meta.env.VITE_BASE_API}${userDetails.profile_picture}`}
+                  className="img-fluid w-12"
+                  alt=""
+                />
+              ) : (
+                <img
+                  src="/images/icons/ProfAvatar.svg"
+                  className="img-fluid w-12"
+                  alt=""
+                />
+              )}
               <div className="flex flex-col leading-none">
-                <h2 className="font-monaBold text-xl">Jane Name</h2>
+                <h2 className="font-monaBold text-xl capitalize">
+                  {userDetails.name}
+                </h2>
                 <span className="font-monaLight text-sm">
-                  jessica.hanson@example.com
+                  {userDetails.email}
                 </span>
               </div>
             </div>
             <div className="flex items-start justify-end">
               <Link
-                to={'/dashboard/user-profile'}
+                to={"/dashboard/user-profile"}
                 className="flexy p-2.5 max-xs:w-5 max-xs:h-5 bg-primary text-white rounded-full"
               >
                 <Pencil className="sm:w-5 sm:h-5 h-4 w-4" />
               </Link>
             </div>
           </div>
-          <div className="flex flex-col lg:flex-row items-center justify-center md:justify-between w-full gap-5 relative mt-6 md:mt-12">
+          {/* <div className="flex flex-col lg:flex-row items-center justify-center md:justify-between w-full gap-5 relative mt-6 md:mt-12">
             <div className="flex rounded-lg border-2 border-dashed border-gray-300 p-3 w-full">
               <div className="flex flex-col w-full space-y-4 text-sm">
                 <div className="flex items-center justify-between gap-5">
@@ -55,26 +81,31 @@ const ProfileOverview = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
 
-      <div className="col-span-12 md:col-span-6 w-full bg-white rounded-lg">
+      {/* <div className="col-span-12 md:col-span-6 w-full bg-white rounded-lg">
         <Link to={'javascript:void(0)'} className="justStartCenter gap-3 p-3">
           <FileDown className="w-7 h-7" />
           <span>Invoices</span>
         </Link>
-      </div>
+      </div> */}
       <div className="col-span-12 md:col-span-6 w-full bg-white rounded-lg">
-        <Link to={'javascript:void(0)'} className="justStartCenter gap-3 p-3">
+        <Link to={"/dashboard/view-order-history"} className="justStartCenter gap-3 p-3">
           <Package className="w-7 h-7" />
-          <span>Order Details</span>
+          <p>
+            Orders: <span className="font-monaBold text-lg">{TotalOrders}</span>
+          </p>
         </Link>
       </div>
       <div className="col-span-12 md:col-span-6 w-full bg-white rounded-lg">
-        <Link to={'javascript:void(0)'} className="justStartCenter gap-3 p-3">
+        <Link to={"/dashboard/view-all-tickets"} className="justStartCenter gap-3 p-3">
           <Ticket className="w-7 h-7" />
-          <span>Tickets</span>
+          <p>
+            Tickets:{" "}
+            <span className="font-monaBold text-lg">{TotalTickets}</span>
+          </p>
         </Link>
       </div>
     </div>
